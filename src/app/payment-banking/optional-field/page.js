972290d42906408cb16869275fe83d963c8624c5
arunchakrawarti <arunmojampur@gmail.com>
2026-07-21 12:51:30 +0530

    feat: add Optionalfied component for managing voucher optional fields

---
+++ src/app/payment-banking/optional-field/page.js
@@ -0,0 +1,15 @@
+import Optionalfied from '@/components/pages/payment-banking/utilities/OptionalField'
+import MainLayout from '@/components/templates/MainLayout'
+import React from 'react'
+
+const page = () => {
+  return (
+    <div>
+      <MainLayout title="Update Voucher Optional Fields">
+        <Optionalfied/>
+      </MainLayout>
+    </div>
+  )
+}
+
+export default page
