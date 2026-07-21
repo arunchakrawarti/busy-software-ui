2e195c15d2e1c4e975baf043a0fb83fc749ca457
arunchakrawarti <arunmojampur@gmail.com>
2026-07-21 11:23:18 +0530

    feat: create PaymentOut page component and update PaymentReceipt title

--- src/app/payment-banking/payment-out/page.js
+++ src/app/payment-banking/payment-out/page.js
@@ -1,12 +1,12 @@
-import PaymentreciptTab from '@/components/pages/payment-banking/payment-reciept/Index'
+import PaymentoutTab from '@/components/pages/payment-banking/payment-out/Index'
 import MainLayout from '@/components/templates/MainLayout'
 import React from 'react'
 
 const page = () => {
   return (
     <div>
-      <MainLayout title="Reciepts">
-        <PaymentreciptTab/>
+      <MainLayout title="Payments">
+        <PaymentoutTab/>
       </MainLayout>
     </div>
   )
