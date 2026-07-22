934975b8fedf603e15c95a598042f4e3a15521ef
arunchakrawarti <arunmojampur@gmail.com>
2026-07-22 15:59:31 +0530

    feat: add Maino5b component and corresponding page for refund data display

---
+++ src/app/gst/return/05b/page.js
@@ -0,0 +1,15 @@
+import Maino5b from '@/components/pages/gst/return/o5b/Index'
+import MainLayout from '@/components/templates/MainLayout'
+import React from 'react'
+
+const page = () => {
+  return (
+    <div>
+      <MainLayout title="On account of refund by recipient of demand export">
+        <Maino5b/>
+      </MainLayout>
+    </div>
+  )
+}
+
+export default page
