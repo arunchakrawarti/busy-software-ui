7bd448fab2148dcf95e0c73478a45164253dae4e
arunchakrawarti <arunmojampur@gmail.com>
2026-07-22 15:59:52 +0530

    feat: add MainSo5 component and page for SEZ Developer data display

---
+++ src/app/gst/return/so5/page.js
@@ -0,0 +1,15 @@
+import MainSo5 from '@/components/pages/gst/return/so5'
+import MainLayout from '@/components/templates/MainLayout'
+import React from 'react'
+
+const page = () => {
+  return (
+    <div>
+      <MainLayout title="SEZ Developer">
+        <MainSo5/>
+      </MainLayout>
+    </div>
+  )
+}
+
+export default page
