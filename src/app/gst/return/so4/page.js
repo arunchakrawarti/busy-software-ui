4f4e79e2cc14e285e528657000764760117291b5
arunchakrawarti <arunmojampur@gmail.com>
2026-07-22 15:59:41 +0530

    feat: add MainSo4 component and page for SEZ Developer data display

---
+++ src/app/gst/return/so4/page.js
@@ -0,0 +1,15 @@
+import MainSo4 from '@/components/pages/gst/return/so4/Index'
+import MainLayout from '@/components/templates/MainLayout'
+import React from 'react'
+
+const page = () => {
+  return (
+    <div>
+      <MainLayout title="SEZ Developer">
+        <MainSo4/>
+      </MainLayout>
+    </div>
+  )
+}
+
+export default page
