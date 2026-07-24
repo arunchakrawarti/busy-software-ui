011e1fd86e86a09884df7e5c812671ec569e29de
arunchakrawarti <arunmojampur@gmail.com>
2026-07-24 12:41:57 +0530

    feat: add CommanMistake component with layout and empty state handling

---
+++ src/app/gst/audit-report/comman-mistake/page.js
@@ -0,0 +1,15 @@
+import CommanMistake from '@/components/pages/gst/audit-reports/CommanMistake'
+import MainLayout from '@/components/templates/MainLayout'
+import React from 'react'
+
+const page = () => {
+  return (
+    <div>
+      <MainLayout title="Comman Mistake">
+        <CommanMistake/>
+      </MainLayout>
+    </div>
+  )
+}
+
+export default page
