836b34a99a5c7ab1f81a69072e980a5ff5bef6d4
arunchakrawarti <arunmojampur@gmail.com>
2026-07-24 11:38:24 +0530

    feat: add TDSCredit component with month selection and layout

---
+++ src/app/gst/register/tds-tcs-credit/page.js
@@ -0,0 +1,15 @@
+import TDSCredit from '@/components/pages/gst/register/TDSCredit'
+import MainLayout from '@/components/templates/MainLayout'
+import React from 'react'
+
+const page = () => {
+  return (
+    <div>
+      <MainLayout>
+        <TDSCredit/>
+      </MainLayout>
+    </div>
+  )
+}
+
+export default page
